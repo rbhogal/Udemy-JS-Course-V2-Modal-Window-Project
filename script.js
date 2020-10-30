22fd2dbd5f597371b7d1cd1b1c7cc8a21c8a6682
rbhogal <rbhogal23gmail.com>
2020-10-30 11:30:26 -0700

    set up query selectors

---
+++ script.js
@@ -0,0 +1,9 @@
+'use strict';
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btn = document.querySelector('.close-modal');
+
+const btnsOpenModal = document.querySelector('.show-modal');
+console.log(btnsOpenModal);
+
